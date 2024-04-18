81123256f48b19aa61db75eaa863339d738677ee
micas <miguelbernardinosousa2002@gmail.com>
2024-04-18 14:10:25 +0100

    website can now update player stats without reloading

--- index.js
+++ index.js
@@ -7,24 +7,23 @@
 // Get Player Stats from JSON file
 document.addEventListener("DOMContentLoaded", function() {
     const updateButton = document.getElementById("updateButton");
-    const dataContainer = document.getElementById("gamesPlayed");
 
     updateButton.addEventListener("click", function() {
         // Fetch JSON data
         fetch("player-stats.json")
             .then(response => response.json())
             .then(data => {
-               // Parse JSON data
-				const gamesPlayed = data.gamesPlayed; 
-				const wins = data.wins;
-				const losses = data.losses;
-				const tournamentWins = data.tournamentWins;
+                // Parse JSON data
+                const gamesPlayed = data.gamesPlayed; 
+                const wins = data.wins;
+                const losses = data.losses;
+                const tournamentWins = data.tournamentWins;
 
-				// Update HTML content
-				document.getElementById('gamesPlayed').innerText = gamesPlayed;
-				document.getElementById('wins').innerText = wins;
-				document.getElementById('losses').innerText = losses;
-				document.getElementById('tournamentWins').innerText = tournamentWins;
+                // Update HTML content
+                document.getElementById('gamesPlayed').innerText = gamesPlayed;
+                document.getElementById('wins').innerText = wins;
+                document.getElementById('losses').innerText = losses;
+                document.getElementById('tournamentWins').innerText = tournamentWins;
             })
             .catch(error => {
                 console.error("Error fetching data:", error);
