d2dd27dde0cb5cc6ab0d1739af129a2291e2f799
micas <miguelbernardinosousa2002@gmail.com>
2024-05-29 15:42:37 +0100

    chat messages now scroll to bottom when modal is loaded

--- index.js
+++ index.js
@@ -257,3 +257,13 @@ function drawGraph(dataArr) {
     }
     context.stroke();
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+	const chatModal = document.getElementById('chat-modal');
+	const chatWindow = document.getElementById('chat-messages');
+
+	chatModal.addEventListener('shown.bs.modal', function() {
+		chatWindow.scrollTop = chatWindow.scrollHeight;
+		console.log(chatWindow.scrollHeight);
+	});
+});
