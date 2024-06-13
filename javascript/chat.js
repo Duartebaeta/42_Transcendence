6c3c7bd950c48ec65597120ee99a0bd51039dcc3
micas <miguelbernardinosousa2002@gmail.com>
2024-06-13 11:13:58 +0100

    chat modal now scrolls contacts to top

--- javascript/chat.js
+++ javascript/chat.js
@@ -2,8 +2,10 @@
 document.addEventListener("DOMContentLoaded", function() {
 	const chatModal = document.getElementById('chat-modal');
 	const chatWindow = document.getElementById('chat-messages');
+	const contactsWindow = document.getElementById('contacts');
 
 	chatModal.addEventListener('shown.bs.modal', function() {
 		chatWindow.scrollTop = chatWindow.scrollHeight;
+		contactsWindow.scrollTop = 0;
 	});
 });
