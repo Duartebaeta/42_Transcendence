2a20d2465a446cdbfeae45284cd50fbf7c1047ba
micas <miguelbernardinosousa2002@gmail.com>
2024-06-19 12:35:46 +0100

    added missing comments for better readability

--- javascript/game-ui.js
+++ javascript/game-ui.js
@@ -11,11 +11,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	// vs AI Mode
     aiModeBtn.addEventListener('click', function() {
+		// Add Coloring To The Pressed Button
 		aiModeBtn.classList.add('bg-warning');
 		localModeBtn.classList.remove('bg-warning');
 		remoteModeBtn.classList.remove('bg-warning');
 		tournamentModeBtn.classList.remove('bg-warning');
 		
+		// Display Selected Mode Options
 		gameHiddenDiv.classList.add('d-none');
 		createBtn.classList.remove('d-none');
 		joinBtn.classList.add('d-none');
@@ -23,11 +25,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	// Local vs Mode
 	localModeBtn.addEventListener('click', function() {
+		// Add Coloring To The Pressed Button
 		aiModeBtn.classList.remove('bg-warning');
 		localModeBtn.classList.add('bg-warning');
 		remoteModeBtn.classList.remove('bg-warning');
 		tournamentModeBtn.classList.remove('bg-warning');
 
+		// Display Selected Mode Options
 		gameHiddenDiv.classList.add('d-none');
 		createBtn.classList.remove('d-none');
 		joinBtn.classList.add('d-none');
@@ -35,11 +39,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	// Remote vs Mode
 	remoteModeBtn.addEventListener('click', function() {
+		// Add Coloring To The Pressed Button
 		aiModeBtn.classList.remove('bg-warning');
 		localModeBtn.classList.remove('bg-warning');
 		remoteModeBtn.classList.add('bg-warning');
 		tournamentModeBtn.classList.remove('bg-warning');
 
+		// Display Selected Mode Options
 		gameHiddenDiv.classList.add('d-none');
 		createBtn.classList.remove('d-none');
 		joinBtn.classList.remove('d-none');
@@ -47,11 +53,13 @@ document.addEventListener('DOMContentLoaded', function() {
 	
 	// Tournament Mode
 	tournamentModeBtn.addEventListener('click', function() {
+		// Add Coloring To The Pressed Button
 		aiModeBtn.classList.remove('bg-warning');
 		localModeBtn.classList.remove('bg-warning');
 		remoteModeBtn.classList.remove('bg-warning');
 		tournamentModeBtn.classList.add('bg-warning');
 
+		// Display Selected Mode Options
 		gameHiddenDiv.classList.remove('d-none');
 		createBtn.classList.remove('d-none');
 		joinBtn.classList.remove('d-none');
