0012c2d19f9990d9332bc4358ad2b5b079101470
CanIBeFii <filipe250502@gmail.com>
2024-08-22 13:39:09 +0100

    feat: update sign in view to return a Refresh token when succesful

--- users/srcs/user/views/sign_in.py
+++ users/srcs/user/views/sign_in.py
@@ -1,6 +1,6 @@
 import json
 
-from django.http import JsonResponse 
+from django.http import JsonResponse
 
 from django.contrib.auth.hashers import check_password
 
@@ -10,6 +10,8 @@ from django.views import View
 
 from django.db.models import Model
 from user.models import User
+
+from user_management.jwt_manager import RefreshJWTManager
 
 @method_decorator(csrf_exempt, name='dispatch')
 class SignIn(View):
@@ -21,12 +23,22 @@ class SignIn(View):
 			return JsonResponse(status=400, data={'errors': ['Invalid UTF-8 encoded bytes']})
 		except json.JSONDecodeError:
 			return JsonResponse(status=400, data={'errors': ['Invalid JSON data format']})
-		
+
 		try:
-			user = User.objects.get(email=json_request['email'])
+			user_email = json_request.get('email')
+			if user_email is None or user_email == '':
+				return JsonResponse(status=400, data={'errors': ['No email was given']})
+			user = User.objects.get(email=user_email)
 		except User.DoesNotExist:
 			return JsonResponse(status=400, data={'errors': ['No User with such email']})
-		
-		if not check_password(password=json_request['password'], encoded=user.password):
+
+		user_password = json_request.get('password')
+		if user_password is None or user_password == '':
+			return JsonResponse(status=400, data={'errors': ['No password was given(How am i suppose to login without a password)']})
+		if not check_password(password=user_password, encoded=user.password):
 			return JsonResponse(status=400, data={'errors': ['Wrong password']})
-		return JsonResponse(status=200)
+
+		success, jwt_token, errors = RefreshJWTManager.generate_token(user.id)
+		if not success:
+			return JsonResponse(status=500, data={'errors': errors})
+		return JsonResponse(status=200, data={'refresh_token': jwt_token})
