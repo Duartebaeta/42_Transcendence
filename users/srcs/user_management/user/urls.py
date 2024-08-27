2b085b4386e5dd579aec379b5c100b789923d4d0
jbuchadas <jbuchadas@ext.worten.pt>
2024-08-27 09:27:29 +0100

    Add verification email step | Missing redirection page

--- users/srcs/user_management/user/urls.py
+++ users/srcs/user_management/user/urls.py
@@ -4,6 +4,7 @@ from user.views.sign_up import SignUp
 from user.views.sign_in import SignIn
 from user.views.change_username import ChangeUsername
 from user.views.change_password import ChangePassword
+from user.views.activate import Activate
 
 from user_management import settings
 
@@ -12,4 +13,5 @@ urlpatterns = [
 	path('sign-in/', SignIn.as_view(), name='signin'),
 	path('change-username/', ChangeUsername.as_view(), name='changeusername'),
 	path('change-password/', ChangePassword.as_view(), name='changepassword'),
+  path('activate/<uidb64>/<token>/', Activate.as_view(), name='activate'),
 ]
---
+++ users/srcs/user_management/user/views/activate.py
@@ -0,0 +1,28 @@
+
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views import View
+from django.utils.http import urlsafe_base64_decode
+from user.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib import messages
+from django.shortcuts import redirect
+
+@method_decorator(csrf_exempt, name='dispatch')
+class Activate(View):
+	@csrf_exempt
+	def get(self, request, uidb64, token):
+		try:
+			uid = urlsafe_base64_decode(uidb64).decode()
+			user = User._default_manager.get(pk=uid)
+		except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+			user = None
+		if user is not None and default_token_generator.check_token(user, token):
+			user.emailVerified = True
+			user.save()
+			messages.success(request, 'Your account has been activated.')
+		else :
+			messages.error(request, 'The confirmation link was invalid, possibly because it has already been used.')
+		# TODO: return a success page
+		return redirect('signin')
+
