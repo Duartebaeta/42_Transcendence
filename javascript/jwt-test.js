1a0a104b8d09bd61a7061f2d893cc6affaf0636d
micas2002 <miguelbernardinosousa2002@gmail.com>
2024-08-28 14:22:50 +0100

    added some tests for the jwt feature

---
+++ javascript/jwt-test.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+	let jwtBtn = document.getElementById("jwt-test-btn");
+
+	jwtBtn.addEventListener('click', function() {
+		let request = {
+			method: 'GET',
+			url: '',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}
+		
+		fetch(request.url, request)
+		.catch(function(error) {
+			// Handle errors
+			console.error('Error:', error);
+		});
+	})
+});
