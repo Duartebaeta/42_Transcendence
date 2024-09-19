bddd146f9c3b9d4eb3327901fab68cd5b919017c
CanIBeFii <filipe250502@gmail.com>
2024-09-19 16:08:07 +0100

    feat: update urls

--- users/srcs/user/urls.py
+++ users/srcs/user/urls.py
@@ -4,6 +4,7 @@ from user.views.sign_up import SignUp
 from user.views.sign_in import SignIn
 from user.views.change_username import ChangeUsername
 from user.views.change_password import ChangePassword
+from user.views.change_avatar import ChangeAvatar
 from user.views.activate import Activate
 from user.views.refresh_jwt_token import RefreshJwtToken
 
@@ -14,6 +15,7 @@ urlpatterns = [
 	path('sign-in/', SignIn.as_view(), name='signin'),
 	path('change-username/', ChangeUsername.as_view(), name='changeusername'),
 	path('change-password/', ChangePassword.as_view(), name='changepassword'),
+	path('change-avatar/', ChangeAvatar.as_view(), name='changeavatar'),
 	path('refresh_jwt/', RefreshJwtToken.as_view(), name='refreshjwt'),
 	path('activate/<uidb64>/<token>/', Activate.as_view(), name='activate'),
 ]
