159508a344598a99de3739775e4674f02d52e162
micas2002 <miguelbernardinosousa2002@gmail.com>
2024-09-21 23:05:39 +0100

    updated match history request to use the authenticated version of the request and changed the api endpoint to the correct one

--- frontend/javascript/match-history.js
+++ frontend/javascript/match-history.js
@@ -2,30 +2,38 @@ document.addEventListener("DOMContentLoaded", function() {
 	const updateButton = document.getElementById("matchHistoryBtn");
 
 	updateButton.addEventListener("click", function() {
+		var request = {
+			method: 'GET',
+			url: 'http://127.0.0.1:8080/user_stats/match/',
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		};
+		
 		// Fetch JSON data
-		fetch("match-history.json")
-			.then(response => response.json())
-			.then(data => {
-				// Parse JSON data
-				const games = data.games;
-				
-				// Create HTML content
-				let outcomes = '';
-				let scores = '';
-				let opponents = ''
-				games.forEach(game => {
-					outcomes += `<p class="text-light text-center"> ${game.outcome}</p>`;
-					scores += `<p class="text-light text-center">${game.score}</p>`;
-					opponents += `<p class="text-light text-center">Opponent: ${game.opponent}`;
-				});
-
-				// Update HTML content
-				document.getElementById('matchHistoryOutcomes').innerHTML = outcomes;
-				document.getElementById('matchHistoryScores').innerHTML = scores;
-				document.getElementById('matchHistoryOpponents').innerHTML = opponents;
-			})
-			.catch(error => {
-				console.error("Error fetching data:", error);
+		authenticatedRequest(request.url, request)
+		.then(response => response.json())
+		.then(data => {
+			// Parse JSON data
+			const games = data.games;
+			
+			// Create HTML content
+			let outcomes = '';
+			let scores = '';
+			let opponents = ''
+			games.forEach(game => {
+				outcomes += `<p class="text-light text-center"> ${game.outcome}</p>`;
+				scores += `<p class="text-light text-center">${game.score}</p>`;
+				opponents += `<p class="text-light text-center">Opponent: ${game.opponent}`;
 			});
+
+			// Update HTML content
+			document.getElementById('matchHistoryOutcomes').innerHTML = outcomes;
+			document.getElementById('matchHistoryScores').innerHTML = scores;
+			document.getElementById('matchHistoryOpponents').innerHTML = opponents;
+		})
+		.catch(error => {
+			console.error("Error fetching data:", error);
+		});
 	});
 });
