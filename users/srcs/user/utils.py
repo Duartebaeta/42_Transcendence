adca8fdca30b6c0f3936224de04c03c31eed664c
CanIBeFii <filipe250502@gmail.com>
2024-09-30 15:15:05 +0100

    fix : establish connection

--- users/srcs/user/utils.py
+++ users/srcs/user/utils.py
@@ -6,24 +6,26 @@ from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, get_connection
 from django.conf import settings
 
 from user.models import User
 
 def send_verification_email(request, user):
-  from_email = settings.DEFAULT_FROM_EMAIL
-  current_site = get_current_site(request)
-  mail_subject = 'Activate Your 42-Transcendence Account'
-  message = render_to_string('accounts/emails/account_verification_email.html', {
-    'user': user,
-    'domain': current_site.domain,
-    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-    'token': default_token_generator.make_token(user),
-  })
-  to_email = user.email
-  email = EmailMessage(mail_subject, message, from_email, to=[to_email])
-  email.send(fail_silently=False)
+	connection = get_connection()
+	from_email = settings.DEFAULT_FROM_EMAIL
+	current_site = get_current_site(request)
+	mail_subject = 'Activate Your 42-Transcendence Account'
+	message = render_to_string('accounts/emails/account_verification_email.html', {
+		'user': user,
+		'domain': current_site.domain,
+		'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+		'token': default_token_generator.make_token(user),
+		})
+	to_email = user.email
+	email = EmailMessage(mail_subject, message, from_email, to=[to_email])
+	email.send(fail_silently=False)
+	connection.close()
 
 def is_valid_username(username):
 	if username is None or username == '':
