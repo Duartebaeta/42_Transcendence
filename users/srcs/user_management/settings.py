329d147714d8c36f82108bad64fe5b8cb23b8244
CanIBeFii <filipe250502@gmail.com>
2024-10-02 15:36:35 +0100

    feat: now SECRET_KEY is not visable

--- users/srcs/user_management/settings.py
+++ users/srcs/user_management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!5axqs)y-poy)$m8m@n$u+hg=091%cy%ite078p)isc8)u9jw7'
+SECRET_KEY = os.getenv('USER_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
