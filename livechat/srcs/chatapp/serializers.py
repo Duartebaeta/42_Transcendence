3ede0df5b0ac89cf6af63a9220492da391364fe9
micas2002 <miguelbernardinosousa2002@gmail.com>
2024-10-04 17:35:26 +0100

    deleted unused file

--- livechat/srcs/chatapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from chatapp.models import ChatMessage
-
-class ChatMessageSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = ChatMessage
-		fields = ('id', 'user', 'room', 'message', 'date')
