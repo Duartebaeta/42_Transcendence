8f649713a42856e9f5654d5cadb5d6e660ad67aa
CanIBeFii <filipe250502@gmail.com>
2024-10-05 14:37:15 +0100

    feat: add endpoint to get Stats of other users

--- user-game-stats/srcs/user_stats/urls.py
+++ user-game-stats/srcs/user_stats/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from user_stats.views.match import Match
 from user_stats.views.user import User
 from user_stats.views.tournament import Tournament
+from user_stats.views.stats import Stats
 
 urlpatterns = [
 	path('user/', User.as_view(), name='user'),
 	path('match/', Match.as_view(), name='match'),
 	path('tournament/', Tournament.as_view(), name='tournament'),
+	path('stats/', Stats.as_view(), name='stats'),
 ]
