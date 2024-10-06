50304337426e8445b44f6f7d14251d77b9abca38
CanIBeFii <filipe250502@gmail.com>
2024-10-06 16:43:52 +0100

    feat: add notification endpoint for tournament notify users of games

--- livechat/srcs/chatapp/urls.py
+++ livechat/srcs/chatapp/urls.py
@@ -6,12 +6,14 @@ from chatapp.views.friend import FriendView
 from chatapp.views.chatroom import ChatRoomView
 from chatapp.views.message import MessageView
 from chatapp.views.invite import Invite
+from chatapp.views.notification import Notification
 
 urlpatterns = [
-	path('invite/', Invite.as_view(), name='invite'),
     path('user/', User.as_view(), name='user'),
     path('chatroom/', ChatRoomView.as_view(), name='chatroom'),
     path('message/', MessageView.as_view(), name='message'),
 	path('block/',BlockUser.as_view(), name='block'),
 	path('friend/',FriendView.as_view(), name='friend'),
+	path('invite/', Invite.as_view(), name='invite'),
+	path('notification/', Notification.as_view(), name='notification'),
 ]
